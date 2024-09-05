83c1a334e3e7ff1d8f7a6731a596706cca8dd8b3
viniciusadams98@gmail.com <viniciusadams98@gmail.com>
2024-09-05 12:34:07 +0100

    setting up grid using bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Grid from "@/components/Grid";
 import { Hero } from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 
 
 
@@ -6,7 +8,9 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+       
       <Hero />
+      <Grid/>
       </div>
       </main>
   );
