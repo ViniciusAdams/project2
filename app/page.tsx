808668711a447c47ab70e35ad7c1134c593fb257
viniciusadams98@gmail.com <viniciusadams98@gmail.com>
2024-09-10 23:16:46 +0100

    creating new page (RecentProjects) and importing it to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,18 @@ import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
 
 
 const Home = () => {
   return (
+    
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
       
         <Hero />
         <Grid />
+        <RecentProjects/>
     
       </div>
     </main>
